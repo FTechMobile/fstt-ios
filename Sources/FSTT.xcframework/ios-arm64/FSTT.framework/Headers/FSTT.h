2af4eb59806ed032b2c70fb9181e2afa5ec9704c
linhdh <doanhoai.linh1123@gmail.com>
2024-08-20 15:08:35 +0700

    v1

---
+++ Sources/FSTT.xcframework/ios-arm64/FSTT.framework/Headers/FSTT.h
@@ -0,0 +1,18 @@
+//
+//  FSTT.h
+//  FSTT
+//
+//  Created by Ishipo on 6/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FSTT.
+FOUNDATION_EXPORT double FSTTVersionNumber;
+
+//! Project version string for FSTT.
+FOUNDATION_EXPORT const unsigned char FSTTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSTT/PublicHeader.h>
+
+
